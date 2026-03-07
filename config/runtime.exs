d02684d63832594ae7521005b9c2833cac44b2ec
Chanlito Hem <chanlitohem@gmail.com>
2026-03-07 22:48:32 +0700

    feat(database): enable SSL for production database connections

--- config/runtime.exs
+++ config/runtime.exs
@@ -85,7 +85,9 @@ if config_env() == :prod do
     [
       url: database_url,
       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-      socket_options: maybe_ipv6
+      socket_options: maybe_ipv6,
+      ssl: true,
+      ssl_opts: [verify: :verify_peer, cacerts: :public_key.cacerts_get()]
     ]
   end
 
